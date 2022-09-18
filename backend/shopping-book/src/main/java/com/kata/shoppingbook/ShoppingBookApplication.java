5b9956a62ad354c94314ff7c8057fb3709c754af
2022-DEV3-059 <temporal-dev@nero.mg>
2022-09-18 16:28:49 +0400

    Setting up the project

---
+++ backend/shopping-book/src/main/java/com/kata/shoppingbook/ShoppingBookApplication.java
@@ -0,0 +1,13 @@
+package com.kata.shoppingbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingBookApplication.class, args);
+	}
+
+}
