3dfd5ec32b0377f00458c20741f38a0ff466d071
2022-DEV3-059 <temporal-dev@nero.mg>
2022-09-20 11:53:33 +0400

    [frontend] : state variables for books component

--- frontend/shopping-book/src/components/product/books/books.component.js
+++ frontend/shopping-book/src/components/product/books/books.component.js
@@ -1,28 +1,36 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Book from '../book/book.component';
 import './books.component.css';
 
 export default function Books() {
 
-    const books = [
-        {
-            bookId: 1,
-            title: "Book title 1",
-            releaseYear: 2020,
-            author: {authorId : 1 , authorName : "author name 1"},
-            coverImage: "https://raw.githubusercontent.com/stephane-genicot/katas/master/images/Kata_DevelopmentBooks_CleanCode.png",
-            price: 50
-        },
-        {
-            bookId: 2,
-            title: "Book title 2",
-            releaseYear: 2022,
-            author: {authorId : 2 , authorName : "author name 2"},
-            coverImage: "https://raw.githubusercontent.com/stephane-genicot/katas/master/images/Kata_DevelopmentBooks_CleanCoder.png",
-            price: 70
-        },
-    ]
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        const books = [
+            {
+                bookId: 1,
+                title: "Book title 1",
+                releaseYear: 2020,
+                author: {authorId : 1 , authorName : "author name 1"},
+                coverImage: "https://raw.githubusercontent.com/stephane-genicot/katas/master/images/Kata_DevelopmentBooks_CleanCode.png",
+                price: 50
+            },
+            {
+                bookId: 2,
+                title: "Book title 2",
+                releaseYear: 2022,
+                author: {authorId : 2 , authorName : "author name 2"},
+                coverImage: "https://raw.githubusercontent.com/stephane-genicot/katas/master/images/Kata_DevelopmentBooks_CleanCoder.png",
+                price: 70
+            },
+        ];
+        
+        setBooks(books);
+
+    }, [])
+
 
     const bookComponent = books.map( b => {
         return (
