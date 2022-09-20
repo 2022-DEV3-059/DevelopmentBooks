792e46766a6a15641c2b86159a0a14d388c079f5
2022-DEV3-059 <temporal-dev@nero.mg>
2022-09-20 14:00:53 +0400

    [frontend] : routes

--- frontend/shopping-book/src/App.js
+++ frontend/shopping-book/src/App.js
@@ -4,11 +4,19 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Books from './components/product/books/books.component';
 import Cart from './components/cart/cart.component';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/page/home.component';
+
 function App() {
   return (
-    <div className='container'>
-        <Cart/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} >
+          <Route index path='cart' element={<Cart />} />
+          <Route path='books' element={<Books/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/shopping-book/src/components/page/header.component.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <div className='container'>
+        <ul className="nav justify-content-end" style={{padding: "20px 0"}}>
+            <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/books">BOOKS</Link>
+            </li>
+            <li className="nav-item">
+                <Link type="button" className="btn btn-primary position-relative" to="/cart">
+                    CART <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">1</span>
+                </Link>
+            </li>
+        </ul>
+    </div>
+    
+  )
+}
